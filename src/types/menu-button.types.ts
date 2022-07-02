f5f09d759ceb0571ecbe4fa274578278978ec5e3
Degreet <degreetpro@gmail.com>
2022-07-02 10:02:55 +0300

    menu button types, web app info type

---
+++ src/types/menu-button.types.ts
@@ -0,0 +1,22 @@
+import { IWebAppInfo } from './web-app.types';
+
+export type MenuButtonTypes = 'default' | 'commands' | 'web_app';
+export type MenuButton = IMenuButtonCommands | IMenuButtonWebApp | IMenuButtonDefault;
+
+export interface IMenuButtonDefaultOptions {
+  type: MenuButtonTypes;
+}
+
+export interface IMenuButtonCommands extends IMenuButtonDefaultOptions {
+  type: 'commands';
+}
+
+export interface IMenuButtonWebApp extends IMenuButtonDefaultOptions {
+  type: 'web_app';
+  text: string;
+  web_app?: IWebAppInfo;
+}
+
+export interface IMenuButtonDefault extends IMenuButtonDefaultOptions {
+  type: 'default';
+}
