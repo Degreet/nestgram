08166f1ea3b17a98fe7e1ce1093c24c90e527239
Degreet <degreetpro@gmail.com>
2022-07-06 17:41:35 +0300

    custom getter for state store

--- src/classes/State/StateStore.ts
+++ src/classes/State/StateStore.ts
@@ -3,7 +3,14 @@ import { IStateInfo } from '../../types/state.types';
 export class StateStore<T = any> {
   protected states: IStateInfo<T>[] = [];
 
+  /**
+   * If you set a custom getter, when the user wants to get the store, he will ask your method. Must return object (state)
+   * */
+  customGet: (userId: number) => T | any;
+
   getStore(userId: number): T {
+    if (this.customGet) return this.customGet(userId);
+
     let state: IStateInfo<T> | undefined = this.states.find(
       (stateInfo: IStateInfo<T>): boolean => stateInfo.userId === userId,
     );
