9fa094d19a91bfcb0c5694e72250dc92b3165400
Degreet <degreetpro@gmail.com>
2022-07-07 14:42:01 +0300

    update hint for @GetState property decorator

--- src/decorators/property/state.decorator.ts
+++ src/decorators/property/state.decorator.ts
@@ -1,5 +1,6 @@
 /**
  * Get state. You can use it to store user data
+ * @see https://degreetpro.gitbook.io/nestgram/advenced/states
  * */
 export function GetState(): PropertyDecorator {
   return (target: any, key: string): void => {
