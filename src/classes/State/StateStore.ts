27730e7067bb8f1c16d2d243cb54193a68a1edf7
Degreet <degreetpro@gmail.com>
2022-07-11 17:36:36 +0300

    custom store setter

--- src/classes/State/StateStore.ts
+++ src/classes/State/StateStore.ts
@@ -1,4 +1,4 @@
-import { CustomGetter, IStateInfo } from '../../types/state.types';
+import { CustomGetter, CustomSetter, IStateInfo } from '../../types/state.types';
 
 export class StateStore<T = any> {
   protected states: IStateInfo<T>[] = [];
@@ -10,11 +10,25 @@ export class StateStore<T = any> {
   private customGetter: CustomGetter;
 
   /**
+   * If you want to set a custom setter, use .setCustomSetter method
+   * */
+  private customSetter: CustomSetter;
+
+  /**
    * If you set a custom getter, when the user wants to get the store, he will ask your method
    * @param customGetter Custom getter you want to save. Your getter can take user id, params from the handler, and default value as arguments. Getter must return object (state)
    * */
   setCustomGetter(customGetter: CustomGetter): true {
     this.customGetter = customGetter;
+    return true;
+  }
+
+  /**
+   * If you set a custom setter, when the user wants to change the store, he will alert your method
+   * @param customSetter Custom setter you want to save. Your setter can take user id, key that will be changed, new value, and params from the handler
+   * */
+  setCustomSetter(customSetter: CustomSetter): true {
+    this.customSetter = customSetter;
     return true;
   }
 
@@ -28,6 +42,8 @@ export class StateStore<T = any> {
   }
 
   async getStore(userId: number, params?: any): Promise<T> {
+    let state: T;
+
     if (this.customGetter) {
       let result: T;
 
@@ -37,19 +53,28 @@ export class StateStore<T = any> {
         result = this.customGetter(userId, params, this.defaultValue);
       }
 
-      return result;
+      state = result;
+    } else {
+      let stateInfo: IStateInfo<T> | undefined = this.states.find(
+        (stateInfo: IStateInfo<T>): boolean => stateInfo.userId === userId,
+      );
+
+      if (!stateInfo) {
+        stateInfo = { userId, state: (this.defaultValue || {}) as T };
+        this.states.push(stateInfo);
+      }
+
+      state = stateInfo.state;
     }
 
-    let state: IStateInfo<T> | undefined = this.states.find(
-      (stateInfo: IStateInfo<T>): boolean => stateInfo.userId === userId,
-    );
+    state = new Proxy<any>(state, {
+      set: (target: any, name: string, value: any): true => {
+        if (this.customSetter) this.customSetter(userId, name, value, params);
+        return true;
+      },
+    });
 
-    if (!state) {
-      state = { userId, state: (this.defaultValue || {}) as T };
-      this.states.push(state);
-    }
-
-    return state.state;
+    return state;
   }
 }
 
--- src/types/state.types.ts
+++ src/types/state.types.ts
@@ -1,5 +1,12 @@
 export type CustomGetter = (userId: number, params?: any, defaultValue?: any) => Promise<any> | any;
 
+export type CustomSetter = (
+  userId: number,
+  key: string,
+  value: any,
+  params?: any,
+) => Promise<any> | any;
+
 export interface IStateInfo<T> {
   userId: number;
   state: T;
