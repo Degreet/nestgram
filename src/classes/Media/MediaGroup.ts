6f74064bcca968853f9d8b460b58ce28244745da
Degreet <degreetpro@gmail.com>
2022-07-11 17:19:16 +0300

    media group rest

--- src/classes/Media/MediaGroup.ts
+++ src/classes/Media/MediaGroup.ts
@@ -2,11 +2,14 @@ import { InputSupportedMedia } from '../../types';
 import { Media } from './Media';
 
 export class MediaGroup extends Media {
+  mediaGroup: InputSupportedMedia[];
+
   /**
    * Creates media group
-   * @param mediaGroup group content
+   * @param mediaGroup Group content
    * */
-  constructor(public readonly mediaGroup: InputSupportedMedia[]) {
+  constructor(...mediaGroup: InputSupportedMedia[]) {
     super('path', '');
+    this.mediaGroup = mediaGroup;
   }
 }
