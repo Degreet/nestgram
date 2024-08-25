6da416ca2517b415559942d4b9aa72794d50d751
Degreet <degreetpro@gmail.com>
2024-08-25 09:50:47 +0300

    Create middleware pipeline

--- lib/dispatcher/middleware.service.ts
+++ lib/dispatcher/middleware.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { NestgramMiddleware } from '../types/NestgramMiddleware';
-import { ExternalContextCreator } from '@nestjs/core';
-import { HandlerParamsFactory } from '../factories/params.factory';
+import { Injectable, Logger, Type } from '@nestjs/common';
+import { ExternalContextCreator, ModuleRef } from '@nestjs/core';
+
+import { HandlerParamsFactory } from '../factories';
 import { Metadata } from '../enums';
+import { NestgramMiddleware } from '../types';
 
 @Injectable()
 export class MiddlewareService {
@@ -12,17 +13,42 @@ export class MiddlewareService {
 
   constructor(
     private readonly externalContextCreator: ExternalContextCreator,
+    private readonly moduleRef: ModuleRef,
   ) {}
 
-  public execute(middleware: NestgramMiddleware, name?: string) {
-    this.logger.debug(`Executing ${name} middleware`);
-    const callback = this.externalContextCreator.create(
+  public createContext(middleware: NestgramMiddleware) {
+    return this.externalContextCreator.create(
       middleware,
       middleware.use,
       middleware.use.name,
       Metadata.PARAMS,
       this.paramsFactory,
     );
-    return callback();
+  }
+
+  public runMiddlewarePipeline(
+    middlewares: Type<NestgramMiddleware>[],
+    args: any[],
+    callback: () => any | Promise<any>,
+    index = 0,
+  ) {
+    if (index < middlewares.length) {
+      const instance = this.moduleRef.get(middlewares[index]);
+      const middlewareName = middlewares[index].name;
+
+      this.logger.debug('Executing ' + middlewareName);
+
+      const handler = this.createContext(instance);
+      return handler(...args, () => {
+        return this.runMiddlewarePipeline(
+          middlewares,
+          args,
+          callback,
+          index + 1,
+        );
+      });
+    } else {
+      return callback();
+    }
   }
 }
